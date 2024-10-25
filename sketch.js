768c1181cf2ab82ee68b8f07b9d7397d45813ac2
virginia-nelson <vmn6772@nyu.edu>
2024-10-25 17:35:17 -0400

    trying to figure out how to switch scenes. doing the boolean method but it is not working

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
 let circles = [];
+let balloon;
+let inScene1 = true;
+
 function setup(){
   createCanvas(900,900);
+  balloon = height
+
 }
 
 function draw(){
+  print(inScene1);
   background(220);
 
   let x = random(width);
@@ -13,27 +19,44 @@ function draw(){
   let growthRate = random(1,3);
 
   let circleColor = color(random(255), random(255), random(255), 150);
+ 
+  while (inScene1){
+    print(balloon);
+    fill(255);
+    ellipse(width/2, balloon, 100, 50);
+    balloon -= 10;
+
+    if(balloon < 0){
+        inScene = false;
+        balloon = height;
+    }
+  }
 
   circles.push([x,y,size,growthRate,circleColor]);
   for(let i = circles.length-1; i >= 0; i--){
     let circle = circles[i];
     fill(circle[4]);
     noStroke;
-    ellipse(circle[0],circle[1],circle[2]);
+    ellipse(circle[0],circle[1],circle[2]); 
 
     circle[2] += circle[3];
-    if (circle[2] > 200){
-      circles.splice(i,1);
-    }
+      if (circle[2] > 200){
+        circles.splice(i,1);
+      }
   }
+  //researching more about what noise() does, i tried to look online for "fluid shapes to make in p5.js"
   stroke(0);
   beginShape();
   for (let x = 0; x < width; x++) {
     let y = noise(x * 0.01, frameCount * 0.01) * height;
     vertex(x, y);
   }
-  endShape();
+   endShape();
+    
 }
+
+
+
 
 
 
